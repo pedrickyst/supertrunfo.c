2f7ee4c50f4a4272e59e144bdbc905a5c3986a25
Pedro Oliveira <pedricky63@gmail.com>
2025-03-21 16:09:50 -0300

    Criar teste.c

---
+++ teste.c
@@ -0,0 +1,30 @@
+#includ <Studio.h>
+
+int main(){
+
+int descarga = 71;
+int etiquetagem = 106;
+int conferencia = 55;
+int guarda = 170;
+
+int paletes = 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100;
+
+int soma, subtracao, divisao, multiplicao, resultado;
+
+int minutos1 = 60;
+int minutos2 = 60;
+int Tempo total;
+
+printf("Calculando descarga: \n");
+scanf("Tempo DESCARGA: \n");
+scanf("Números de PALETES: \n");
+scanf("Tempo divisor: \n");
+scanf("Tempo divisor: \n");
+
+printf("TEMPO DESCARGA: \n", descarga x paletes / minutos1 / minutos2 = resultado);
+printf("TEMPO ETIQUETAGEM: \n", etiquetagem x paletes / minutos1 / minutos2 = resultado);
+printf("TEMPO CONFERÊNCIA: \n", conferencia x paletes / minutos1 / minutos2 = resultado);
+printf("TEMPO GUARDA: \n", guarda x paletes / minutos1 / minutos2 = resultado);
+
+
+}
